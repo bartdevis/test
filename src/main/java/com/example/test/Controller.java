ac618c75be1111ba367351943f1cfd62ad4690e1
Bart Devis <bart.devis2@gmail.com>
2023-06-24 05:52:50 +0200

    change controller

--- src/main/java/com/example/test/Controller.java
+++ src/main/java/com/example/test/Controller.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/helloWorld")
 public class Controller {
 
-    @GetMapping
+    @GetMapping("/helloWorld")
     public String getHelloWorld(){
         return "hello world";
     }
